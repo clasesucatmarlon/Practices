4a44290ceb697d618766672b08e5b1eb9323319b
clasesucatmarlon <clasesucatmarlon@gmail.com>
2021-04-17 10:26:04 -0500

    Ejercicio_08

---
+++ python/Udemy-Python3-desde-cero-45-Ejercicios/S4-EjercicosBasicos_08.py
@@ -0,0 +1,18 @@
+""" Sección 4: Ejercicios Básicos """
+
+"""
+Programa que pregunte dos números y luego mostrar el menos de ellos  
+"""
+
+n1 = float(input("Primer número: "))
+n2 = float(input("Segundo número: "))
+
+if (n1 < n2):
+    print("El menor número entre ", n1 , " y ", n2, " es: ", n1)
+else:
+    print("El menor número entre ", n1 , " y ", n2, " es: ", n2)
+
+
+
+
+
