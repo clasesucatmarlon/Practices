faf7c936a8da58a5c740bb194d34a6f78fcde833
clasesucatmarlon <clasesucatmarlon@gmail.com>
2021-04-17 10:24:00 -0500

    Ejercicio_03

---
+++ python/Udemy-Python3-desde-cero-45-Ejercicios/S4-EjercicosBasicos_03.py
@@ -0,0 +1,9 @@
+""" Sección 4: Ejercicios Básicos """
+
+"""
+Programa que pregunte el nombre del usuario en la cónsola
+y muestre la cadena Hola <nombre>
+"""
+
+nombre = input("Escribe tu nombre: ")
+print("Hola ", nombre)
