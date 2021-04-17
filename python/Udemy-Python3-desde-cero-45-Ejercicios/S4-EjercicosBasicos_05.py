05136297c3f3814dc575efc67ed2cf76a0682308
clasesucatmarlon <clasesucatmarlon@gmail.com>
2021-04-17 10:24:37 -0500

    Ejercicio_05

---
+++ python/Udemy-Python3-desde-cero-45-Ejercicios/S4-EjercicosBasicos_05.py
@@ -0,0 +1,12 @@
+""" Sección 4: Ejercicios Básicos """
+
+"""
+Programa que pregunte el nombre del usuario.  Imprimir 
+<NOMBRE> tiene <n> letras.  
+"""
+
+nombre = input("Escribe tu nombre: ")
+
+
+
+print(nombre.upper(), " tiene ", len(nombre), " letras")
