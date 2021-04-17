76f21d4486a29ede21d5d39b05c4bc489f492932
clasesucatmarlon <clasesucatmarlon@gmail.com>
2021-04-17 10:24:51 -0500

    Ejercicio_06

---
+++ python/Udemy-Python3-desde-cero-45-Ejercicios/S4-EjercicosBasicos_06.py
@@ -0,0 +1,15 @@
+""" Sección 4: Ejercicios Básicos """
+
+"""
+Programa que pregunte números de horas trabajadas y coste por hora.  
+Imprimir el pago devengado .  
+"""
+
+ht = int(input("Horas trabajadas: "))
+ph = float(input("pago ppr hora: "))
+
+tp = ht * ph
+
+
+
+print("El pago es: ", tp)
