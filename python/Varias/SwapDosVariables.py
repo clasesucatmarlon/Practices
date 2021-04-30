a8cbb26d0c1938a8cce859bf0f67c6d1f0cb0f0d
clasesucatmarlon <clasesucatmarlon@gmail.com>
2021-04-30 12:24:45 -0500

    added file for swap two numbers only with math operations

---
+++ python/Varias/SwapDosVariables.py
@@ -0,0 +1,12 @@
+# Hacer swap sólo con las operaciones básicas de suma y diferencia
+
+m = 1
+o = 4
+
+#swap
+m = m + o    # m = 4 + 1 = 5
+o = m - o    # o = 5 - 4 = 1
+m = m - o    # m = 5 - 1 = 4
+
+print("Nuevo valor de m: ", m)  # Nuevo valor de m: 4
+print("Nuevo valor de o: ", o)  # Nuevo valor de o: 1 
