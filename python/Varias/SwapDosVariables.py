3fb96fc350ec7be784be0fbfa88723e4121712ab
clasesucatmarlon <clasesucatmarlon@gmail.com>
2021-04-30 19:36:23 -0500

    swap between two numers

--- python/Varias/SwapDosVariables.py
+++ python/Varias/SwapDosVariables.py
@@ -1,5 +1,4 @@
-# Hacer swap sólo con las operaciones básicas de suma y diferencia
-
+# Swap with mathematics operations
 m = 1
 o = 4
 
@@ -8,5 +7,12 @@ m = m + o    # m = 4 + 1 = 5
 o = m - o    # o = 5 - 4 = 1
 m = m - o    # m = 5 - 1 = 4
 
-print("Nuevo valor de m: ", m)  # Nuevo valor de m: 4
-print("Nuevo valor de o: ", o)  # Nuevo valor de o: 1 
+print("New value of m: ", m)  # New value of m: 4
+print("New value of o: ", o)  # New value of o: 1 
+
+# otro método
+m = 1
+o = 4
+m, o = o, m
+print("New value of m: ", m)  # New value of m: 4
+print("New value of o: ", o)  # New value of o: 1 
