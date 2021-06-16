0a4dd3148ee50f65526940296d9baaaca4689fec
clasesucatmarlon <clasesucatmarlon@gmail.com>
2021-06-16 09:19:47 -0500

    JS

--- Javascript/test1.js
+++ Javascript/test1.js
@@ -1,4 +1,4 @@
-/* Lenguaje: Javascript
+/* Languaje: Javascript
 Program that: 
 add 2 for every odd number   / +2 por cada num impar
 add 6 for every even number  / +6 por cada num par
@@ -6,25 +6,25 @@ add 6 for every 9 you find   / +8 por cada 9 encontrado
 */
 
 //let valores = [ 1, 5, 7, 8, 14 ]
-let valores = [ 9, 2, 5, 19 ];
+let values = [ 9, 2, 5, 19 ];
 
-function Recorrer (valores) {
-    let suma = 0;
-    total = valores.length
+function Run (values) {
+    let add = 0;
+    total = values.length
 
     for (let i = 0; i < total; i++) {
-        if (valores[i] === 9) {
-            suma += 8;
+        if (values[i] === 9) {
+            add += 8;
         } 
-        else if (valores[i] % 2 === 1) {
-            suma += 2;
+        else if (values[i] % 2 === 1) {
+            add += 2;
         } 
-        else if (valores[i] % 2 === 0) {
-            suma += 6;
+        else if (values[i] % 2 === 0) {
+            add += 6;
         }
     }
-    return suma;
+    return add;
 }
 
-total = Recorrer(valores);
-console.log("El total da: ", total); // 8 + 6 + 2 + 2
+total = Run(values);
+console.log("Total = ", total); // 8 + 6 + 2 + 2
---
+++ Javascript/test2.js
@@ -0,0 +1,21 @@
+let rank = "A";
+
+
+if ( rank === 'A' ) {
+    console.log('Obtiene bono para un viaje.....');
+    } else if ( rank === 'B' ) {
+        console.log('Obtiene bono para compras.....');
+    } else if ( rank === 'C' ) {
+        console.log('Obtiene tiempo libre para ver TV....');
+    } else {
+        console.log('Tiene Review con el líder técnico....');
+    }
+
+
+
+const array = [
+    {'A': 'Obtiene bono para un viaje.....'},
+    {'B': 'Obtiene bono para compras.....'},
+    {'C': 'Obtiene tiempo libre para ver TV....'},
+    {'D': 'Tiene Review con el líder técnico....'},
+]
